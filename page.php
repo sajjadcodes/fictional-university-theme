2650526506d847add00796a78c81c48bd968de6a
sajadhussain <sakraan01@gmail.com>
2021-11-21 00:16:03 +0500

    blog pagination and single blog post template

--- page.php
+++ page.php
@@ -7,15 +7,15 @@ while(have_posts()){
     the_post();
     ?>
 
-<div class="page-banner">
+  <div class="page-banner"> 
       <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('images/ocean.jpg');?>)"></div>
-      <div class="page-banner__content container container--narrow">
-        <h1 class="page-banner__title"><?php the_title();?></h1>
-        <div class="page-banner__intro">
-          <p> Do not forget to make it dynamic later</p>
+        <div class="page-banner__content container container--narrow">
+            <h1 class="page-banner__title"><?php the_title();?></h1>
+            <div class="page-banner__intro">
+              <p> Do not forget to make it dynamic later</p>
+            </div>
         </div>
-      </div>
-    </div>
+ </div>
 
     <div class="container container--narrow page-section">
 
