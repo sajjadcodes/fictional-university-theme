6e5ae013e77ed6f08382f6754f5b0e90d58c8c81
sajadhussain <sakraan01@gmail.com>
2021-11-21 00:55:08 +0500

    improvement the archive title. Replaced the whole condtions with single template tag. the_archive_title()

--- archive.php
+++ archive.php
@@ -6,13 +6,17 @@
 <div class="page-banner"> 
   <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('images/ocean.jpg');?>)"></div>
     <div class="page-banner__content container container--narrow">
-        <h1 class="page-banner__title"><?php if(is_category(  )){
-            single_cat_title();
-            }
-            elseif(is_author()){
-                 echo"Post By:"; the_author();
-            }
-            ?>
+        <h1 class="page-banner__title">
+        <?php
+        //  if(is_category(  )){
+        //     single_cat_title();
+        //     }
+        //     elseif(is_author()){
+        //          echo"Post By:"; the_author();
+        //     }
+        // replacing the above code with the the_archive_title(); newer function
+        the_archive_title();
+        ?>
         </h1>
         <div class="page-banner__intro">
           <p>Keep up with our latest news.</p>
