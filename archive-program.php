5a76dd41b005db7abfaa6895549c3a231b047c43
sajadhussain <sakraan01@gmail.com>
2021-11-22 08:09:26 +0500

    archive template for programs and URL based query maninplulated

---
+++ archive-program.php
@@ -0,0 +1,44 @@
+
+
+
+<?php get_header(); ?>
+
+<div class="page-banner"> 
+  <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('images/ocean.jpg');?>)"></div>
+    <div class="page-banner__content container container--narrow">
+        <h1 class="page-banner__title">
+                <?php _e('All Programs', 'fictional-uni');?>
+                
+        </h1>
+        <div class="page-banner__intro">
+          <p>Here are our latest programs list</p>
+        </div>
+    </div>
+</div>
+
+
+<!-- Main sectin -->
+<div class="container container--narrow page-section">
+       <ul class="link-list min-list">
+       <?php 
+            
+            while(have_posts()){
+                    the_post(); ?>
+
+                 <li><a href="<?php the_permalink();?>"><?php the_title();?></a></li>
+               <?php      
+            }
+
+            // echo the_posts_pagination(  );
+            echo paginate_links(  );
+        
+        ?>
+        </ul>
+        <hr class="section-break">
+        <p>Looking for a recap of past events? <a href="<?php echo site_url('/past-events');?>">check out our past Events archive</a></p>
+</div>
+
+
+<?php get_footer(); ?>
+
+
--- functions.php
+++ functions.php
@@ -29,6 +29,14 @@ function university_features(){
 
 function university_adjust_queries($query){
 
+    if(!is_admin() AND is_post_type_archive( 'program' ) AND is_main_query()){
+
+            $query->set('orderby', 'title');
+            $query->set('order', 'ASC' );
+            $query->set('order', 'ASC' );
+            $query->set('posts_per_page', -1);
+    }
+
             $today = date('Ymd');
             if(!is_admin() && is_post_type_archive( 'event' ) && $query->is_main_query()){
 
