3322d69ca1d06c247b7183f5b35dcd65630597cf
sajadhussain <sakraan01@gmail.com>
2021-11-23 04:12:52 +0500

    js automatiion with gitignore file

--- archive-event.php
+++ archive-event.php
@@ -28,20 +28,9 @@
             
 
             while(have_posts()){
-                    the_post(); ?>
+                    the_post(); 
 
-                    <div class="event-summary">
-                        <a class="event-summary__date t-center" href="<?php the_permalink();?>">
-                          <span class="event-summary__month"><?php the_time('M');?></span>
-                          <span class="event-summary__day"><?php the_time('d');?></span>
-                        </a>
-                        <div class="event-summary__content">
-                          <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink();?>"><?php the_title();?></a></h5>
-                          <p><?php echo wp_trim_words(get_the_content(),20 );?> <a href="<?php the_permalink(); ?>" class="nu gray">Learn more</a></p>
-                        </div>
-                    </div>
-            
-               <?php      
+                get_template_part('template-parts/event', 'excerpt');
             }
 
             // echo the_posts_pagination(  );
