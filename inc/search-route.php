3b28192ed963f629d169c9609bb7fb16a245a586
Sajjad Hussain <sakraan01@gmail.com>
2022-07-15 22:49:54 +0500

    Update search-route.php

--- inc/search-route.php
+++ inc/search-route.php
@@ -2,7 +2,6 @@
 
 add_action('rest_api_init','universityRegisterSearch');
 function universityRegisterSearch(){
-
     register_rest_route( 'university/v1', 'search', array(
         'methods'           =>WP_REST_SERVER::READABLE,
         'callback'          =>'universitySearchResults'
